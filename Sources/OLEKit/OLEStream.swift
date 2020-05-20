8e0417846f3e0f96c41469156e7c7836c5c12f2a
Max Desiatov <max@desiatov.com>
2020-05-20 17:18:24 +0100

    Add sector iteration to OLEStream

--- Sources/OLEKit/OLEStream.swift
+++ Sources/OLEKit/OLEStream.swift
@@ -9,9 +9,8 @@ extension DataStream {
     sectorSize: UInt16,
     fat: [UInt32]
   ) throws {
-    if totalSize == 0 && sectorID == SectorID.endOfChain.rawValue {
-      throw OLEError.invalidEmptyOLEStream
-    }
+    guard !(totalSize == 0 && sectorID == SectorID.endOfChain.rawValue)
+    else { throw OLEError.invalidEmptyOLEStream }
 
     let sectorSize = UInt64(sectorSize)
     let fatCount = UInt64(fat.count)
@@ -22,6 +21,19 @@ extension DataStream {
     // sectors in the given FAT:
     guard numberOfSectors < fatCount
     else { throw OLEError.streamTooLarge(actual: numberOfSectors, expected: fatCount) }
+
+    var currentSectorID = sectorID
+    for _ in 0..<numberOfSectors {
+      guard currentSectorID != SectorID.endOfChain.rawValue else {
+        if let totalSize = totalSize {
+          // This means that the stream is smaller than declared:
+          throw OLEError.incompleteOLEStream(start: sectorID, expected: numberOfSectors)
+        } else {
+          // Reached end of chain for a stream with unknown size
+          break
+        }
+      }
+    }
 
     data = Data()
   }
