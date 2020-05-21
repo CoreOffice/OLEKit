3c406be6ef73eeb5fc5c633c5cdbe42cd26e86f5
Max Desiatov <max@desiatov.com>
2020-05-21 20:59:50 +0100

    Test directory entry types in OLEKitTests

--- Tests/OLEKitTests/OLEKitTests.swift
+++ Tests/OLEKitTests/OLEKitTests.swift
@@ -25,20 +25,25 @@ final class OLEKitTests: XCTestCase {
     XCTAssertEqual(ole.header.miniSectorSize, 64)
     XCTAssertEqual(ole.root.name, "Root Entry")
     XCTAssertEqual(ole.root.streamSize, 1920)
+    XCTAssertEqual(ole.root.type, .root)
     XCTAssertEqual(ole.root.children.map { $0.name }, ["EncryptionInfo"])
     XCTAssertEqual(ole.root.children[0].streamSize, 1292)
+    XCTAssertEqual(ole.root.children[0].type, .stream)
     XCTAssertEqual(
       ole.root.children[0].children.map { $0.name },
       ["\u{06}DataSpaces", "EncryptedPackage"]
     )
     XCTAssertEqual(ole.root.children[0].children[0].streamSize, 0)
+    XCTAssertEqual(ole.root.children[0].children[0].type, .storage)
     XCTAssertEqual(ole.root.children[0].children[0].children.map { $0.name }, ["DataSpaceMap"])
     XCTAssertEqual(ole.root.children[0].children[1].streamSize, 8216)
+    XCTAssertEqual(ole.root.children[0].children[1].type, .stream)
     XCTAssertEqual(ole.root.children[0].children[1].children.map { $0.name }, [])
     XCTAssertEqual(
       ole.root.children[0].children[0].children[0].children.map { $0.name },
       ["Version", "DataSpaceInfo"]
     )
     XCTAssertEqual(ole.root.children[0].children[0].children[0].streamSize, 112)
+    XCTAssertEqual(ole.root.children[0].children[0].children[0].type, .stream)
   }
 }
